4547e911550e492cf65a54751ac4beca58c75428
Ivan Herve <ivanherve1411@gmail.com>
2021-04-28 14:53:29 +0200

    adding an existing parent

--- src/members-pages/modals/addParent.jsx
+++ src/members-pages/modals/addParent.jsx
@@ -1,7 +1,16 @@
-import { useState } from "react";
-import { Alert, Button, Col, Form, Modal, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import {
+  Alert,
+  Button,
+  Col,
+  Form,
+  Modal,
+  Nav,
+  Row,
+  Tab,
+} from "react-bootstrap";
 import swal from "sweetalert";
-import { ENDPOINT, postAuthRequest } from "../../links/links";
+import { ENDPOINT, getAuthRequest, postAuthRequest } from "../../links/links";
 
 export default function AddParent(props) {
   const token = JSON.parse(sessionStorage.getItem("userData")).token.Api_token;
@@ -12,17 +21,30 @@ export default function AddParent(props) {
   const [email, setEmail] = useState("");
   const [address, setAddress] = useState("");
   const [linkChild, setLinkChild] = useState("");
+  const [listParent, setListParent] = useState([]);
+  const [parentId, setParentId] = useState(0);
+  const [parentSelected, setParentSelected] = useState("");
 
   const addParent = () => {
-    let data = JSON.stringify({
-      StudentId: props.studentId,
-      firstname: firstname.toUpperCase(),
-      lastname: lastname.toUpperCase(),
-      telephone: telephone,
-      email: email,
-      address: address,
-      linkChild: linkChild,
-    });
+    let data;
+    if (parentSelected.length > 0) {
+      //console.log("Ajout d'un parent existant");
+      data = JSON.stringify({
+        StudentId: props.studentId,
+        ParentId: parentId,
+        Name: parentSelected,
+      });
+    } else {
+      data = JSON.stringify({
+        StudentId: props.studentId,
+        firstname: firstname.toUpperCase(),
+        lastname: lastname.toUpperCase(),
+        telephone: telephone,
+        email: email,
+        address: address,
+        linkChild: linkChild,
+      });
+    }
 
     fetch(ENDPOINT("parents/create"), postAuthRequest(data, token))
       .then((r) => r.json())
@@ -35,6 +57,27 @@ export default function AddParent(props) {
       });
   };
 
+  const selectParent = (parent) => {
+    if (listParent.length > 0)
+      listParent.map((p) => {
+        if (p.Name === parent) {
+          setParentId(p.ParentId);
+          setParentSelected(parent);
+        }
+      });
+  };
+
+  useEffect(() => {
+    fetch(ENDPOINT("listparents"), getAuthRequest(token))
+      .then((r) => r.json())
+      .then((r) => {
+        if (r.status) {
+          setListParent(r.response);
+          //setParentId(r.response[0].ParentId);
+        }
+      });
+  }, []);
+
   return (
     <Modal show={props.show} onHide={props.hide} centered size="xl">
       <Modal.Header>
@@ -45,73 +88,149 @@ export default function AddParent(props) {
       */}
 
       <Modal.Body>
-        <Form>
-          <Form.Group as={Row} controlId="addparentfirstname">
-            <Form.Label column sm="2">
-              Prénom*
-            </Form.Label>
-            <Col sm="10">
-              <Form.Control onChange={(e) => setFirstname(e.target.value)} />
+        <Tab.Container id="left-tabs-example" defaultActiveKey="old">
+          <Row>
+            <Col sm={3}>
+              <Nav variant="pills" className="flex-column">
+                <Nav.Item>
+                  <Nav.Link eventKey="old">Ajouter un parent existant</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="new">Ajouter un nouveau parent</Nav.Link>
+                </Nav.Item>
+              </Nav>
             </Col>
-          </Form.Group>
-          <Form.Group as={Row} controlId="addparentlastname">
-            <Form.Label column sm="2">
-              Nom*
-            </Form.Label>
-            <Col sm="10">
-              <Form.Control onChange={(e) => setLastname(e.target.value)} />
+            <Col sm="9">
+              <Tab.Content>
+                <Tab.Pane eventKey="old">
+                  <Form>
+                    <Form.Group as={Row} controlId="addAnExistingParent">
+                      <Form.Label column sm="2">
+                        Parent / Tuteur
+                      </Form.Label>
+                      <Col sm="10">
+                        <Form.Control
+                          as="select"
+                          onChange={(e) => selectParent(e.target.value)}
+                        >
+                          {listParent.length > 0 &&
+                            listParent.map((p) => (
+                              <option key={p.ParentId}>{p.Name}</option>
+                            ))}
+                        </Form.Control>
+                      </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} controlId="selectExistingParent">
+                      <Col sm="12">
+                        <Form.Control
+                          readOnly
+                          value={
+                            parentSelected.length < 1
+                              ? "Aucun parent selectionné ..."
+                              : parentSelected
+                          }
+                        />
+                      </Col>
+                    </Form.Group>
+                  </Form>
+                </Tab.Pane>
+                <Tab.Pane eventKey="new">
+                  <Form>
+                    <Form.Group as={Row} controlId="addparentfirstname">
+                      <Form.Label column sm="2">
+                        Prénom*
+                      </Form.Label>
+                      <Col sm="10">
+                        <Form.Control
+                          onChange={(e) => {
+                            setFirstname(e.target.value);
+                            setParentSelected("");
+                          }}
+                        />
+                      </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} controlId="addparentlastname">
+                      <Form.Label column sm="2">
+                        Nom*
+                      </Form.Label>
+                      <Col sm="10">
+                        <Form.Control
+                          onChange={(e) => {
+                            setLastname(e.target.value);
+                            setParentSelected("");
+                          }}
+                        />
+                      </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} controlId="addparentphone">
+                      <Form.Label column sm="2">
+                        Téléphone*
+                      </Form.Label>
+                      <Col sm="10">
+                        <Form.Control
+                          type="number"
+                          onChange={(e) => {
+                            setTelephone(e.target.value);
+                            setParentSelected("");
+                          }}
+                        />
+                      </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} controlId="addparentemail">
+                      <Form.Label column sm="2">
+                        E-mail
+                      </Form.Label>
+                      <Col sm="10">
+                        <Form.Control
+                          type="email"
+                          onChange={(e) => {
+                            setEmail(e.target.value);
+                            setParentSelected("");
+                          }}
+                        />
+                      </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} controlId="addparentaddress">
+                      <Form.Label column sm="2">
+                        Adresse*
+                      </Form.Label>
+                      <Col sm="10">
+                        <Form.Control
+                          onChange={(e) => {
+                            setAddress(e.target.value);
+                            setParentSelected("");
+                          }}
+                        />
+                      </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} controlId="addparentprofession">
+                      <Form.Label column sm="2">
+                        Profession
+                      </Form.Label>
+                      <Col sm="10">
+                        <Form.Control />
+                      </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} controlId="addparentlink">
+                      <Form.Label column sm="2">
+                        Lien avec l'enfant*
+                      </Form.Label>
+                      <Col sm="10">
+                        <Form.Control
+                          as="textarea"
+                          onChange={(e) => {
+                            setLinkChild(e.target.value);
+                            setParentSelected("");
+                          }}
+                        />
+                      </Col>
+                    </Form.Group>
+                  </Form>
+                </Tab.Pane>
+              </Tab.Content>
             </Col>
-          </Form.Group>
-          <Form.Group as={Row} controlId="addparentphone">
-            <Form.Label column sm="2">
-              Téléphone*
-            </Form.Label>
-            <Col sm="10">
-              <Form.Control
-                type="number"
-                onChange={(e) => setTelephone(e.target.value)}
-              />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} controlId="addparentemail">
-            <Form.Label column sm="2">
-              E-mail
-            </Form.Label>
-            <Col sm="10">
-              <Form.Control
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} controlId="addparentaddress">
-            <Form.Label column sm="2">
-              Adresse*
-            </Form.Label>
-            <Col sm="10">
-              <Form.Control onChange={(e) => setAddress(e.target.value)} />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} controlId="addparentprofession">
-            <Form.Label column sm="2">
-              Profession
-            </Form.Label>
-            <Col sm="10">
-              <Form.Control />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} controlId="addparentlink">
-            <Form.Label column sm="2">
-              Lien avec l'enfant*
-            </Form.Label>
-            <Col sm="10">
-              <Form.Control
-                as="textarea"
-                onChange={(e) => setLinkChild(e.target.value)}
-              />
-            </Col>
-          </Form.Group>
-        </Form>
+          </Row>
+        </Tab.Container>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="light" onClick={() => addParent()}>
