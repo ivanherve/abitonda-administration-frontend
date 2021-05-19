b270c738b1b31b8f9125ad30758a88ef809b276a
Ivan Herve <ivanherve1411@gmail.com>
2021-05-19 11:10:31 +0200

    birthday refractoring

--- src/members-pages/mainComponents/birthday.jsx
+++ src/members-pages/mainComponents/birthday.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
-import { Card, Nav } from "react-bootstrap";
+import moment from "moment";
+import { useState, useEffect } from "react";
+import { Card, Nav, Table } from "react-bootstrap";
+import { ENDPOINT, getAuthRequest, Loading } from "../../links/links";
 
 export default function Birthday(props) {
-  const [key, setKey] = useState("past");
+  const token = JSON.parse(sessionStorage.getItem("userData")).token.Api_token;
+  const [key, setKey] = useState("coming");
   return (
     <div>
       <Card>
@@ -11,14 +14,14 @@ export default function Birthday(props) {
           <Nav
             fill
             variant="tabs"
-            defaultActiveKey="past"
+            defaultActiveKey="coming"
             onSelect={(e) => setKey(e)}
           >
             <Nav.Item>
-              <Nav.Link eventKey="past">Anniversaire Passé</Nav.Link>
+              <Nav.Link eventKey="coming">Anniversaire à venir</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey="coming">Anniversaire à venir</Nav.Link>
+              <Nav.Link eventKey="past">Anniversaire Passé</Nav.Link>
             </Nav.Item>
           </Nav>
         </Card.Header>
@@ -31,6 +34,70 @@ export default function Birthday(props) {
 }
 
 function Bday(props) {
-  if (props.eventkey === "past") return <div>past birthdays</div>;
-  else return <div>coming birthdays</div>;
+  if (props.eventkey === "past") return <PastBday />;
+  else return <ComingBday />;
+}
+
+function PastBday(props) {
+  const token = JSON.parse(sessionStorage.getItem("userData")).token.Api_token;
+  const [bdays, setBdays] = useState([]);
+  useEffect(() => {
+    fetch(ENDPOINT("pastbday"), getAuthRequest(token))
+      .then((r) => r.json())
+      .then((r) => setBdays(r.response));
+  }, []);
+  return (
+    <Table striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th>Élève</th>
+          <th>Date d'anniversaire</th>
+          <th>Âge</th>
+        </tr>
+      </thead>
+      <tbody>
+        {bdays.map((b) => (
+          <tr key={bdays.indexOf(b)}>
+            <td>
+              <strong>{b.Firstname}</strong> {b.Lastname}
+            </td>
+            <td>{moment(b.BirthDay).format("Do MMMM")}</td>
+            <td>{b.age} ans</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
+
+function ComingBday(props) {
+  const token = JSON.parse(sessionStorage.getItem("userData")).token.Api_token;
+  const [bdays, setBdays] = useState([]);
+  useEffect(() => {
+    fetch(ENDPOINT("comingbday"), getAuthRequest(token))
+      .then((r) => r.json())
+      .then((r) => setBdays(r.response));
+  }, []);
+  return (
+    <Table striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th>Élève</th>
+          <th>Date d'anniversaire</th>
+          <th>Âge</th>
+        </tr>
+      </thead>
+      <tbody>
+        {bdays.map((b) => (
+          <tr key={bdays.indexOf(b)}>
+            <td>
+              <strong>{b.Firstname}</strong> {b.Lastname}
+            </td>
+            <td>{moment(b.BirthDay).format("Do MMMM")}</td>
+            <td>{b.age} ans</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
 }
