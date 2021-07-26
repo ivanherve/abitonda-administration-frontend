5f629e8c2027ca4f9380a69ab697d3f21d5bc416
Ivan Herve <ivanherve1411@gmail.com>
2021-07-26 16:48:30 +0200

    students pass to the next class

--- src/members-pages/mainComponents/classes.jsx
+++ src/members-pages/mainComponents/classes.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Button, Card, Col, ListGroup, Nav, Row, Table } from "react-bootstrap";
+import swal from "sweetalert";
 import { ENDPOINT, getAuthRequest, Loading } from "../../links/links";
 
 //const classes = ["TPS", "PS", "MS", "GS", "CP", "CE1"];
@@ -33,6 +34,37 @@ export default function Classe(props) {
       });
   };
 
+  const switchToNextClass = () => {
+    let title = "Êtes-vous sûr ?";
+    let description =
+      'Cette fonctionalité fait passer tous les élèves en classe supérieur! Si un élève ne doit pas passer en classe supérieur, vous pouvez modifier sa classe dans l\'onglet "Élève"';
+    let isOk = {
+      title: title,
+      text: description,
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    };
+    swal(isOk).then((ok) => {
+      if (ok) {
+        // fetch
+        fetch(ENDPOINT("passtonextclass"), getAuthRequest(token))
+          .then((r) => r.json())
+          .then((r) => {
+            if (r.status)
+              swal(
+                "Parfait",
+                "Tous les élèves sont passé en classe supérieur",
+                "success"
+              ).then(() => window.location.reload());
+          });
+        // swal
+      } else {
+        swal("Aucune modification n'a été faite");
+      }
+    });
+  };
+
   //setTimeout(setPresenceLoading(true), 3000);
 
   useEffect(() => {
@@ -57,7 +89,20 @@ export default function Classe(props) {
           </ListGroup>
         </Col>
         <Col>
-          <h2>{selectedClasse}</h2>
+          <Row>
+            <Col>
+              <h2>{selectedClasse}</h2>
+            </Col>
+            <Col>
+              <Button
+                variant="outline-secondary"
+                onClick={() => switchToNextClass()}
+                style={{ width: "100%" }}
+              >
+                Faire passer tout le monde en classe supérieur
+              </Button>
+            </Col>
+          </Row>
           <Card>
             <Card.Header>
               <Nav
@@ -127,7 +172,7 @@ function Presence(props) {
                     <strong>{s.Lastname}</strong> <i>{s.Firstname}</i>
                   </td>
                   <td>
-                    <input type="checkbox" />
+                    <input type="checkbox" disabled />
                   </td>
                 </tr>
               ))
@@ -142,13 +187,23 @@ function Presence(props) {
           </tbody>
         </Table>
         <Card.Footer>
-          <Button>Enregistrer</Button>
+          <Button disabled>Enregistrer</Button>
         </Card.Footer>
       </div>
     );
 }
 
 function Informations(props) {
+  const token = JSON.parse(sessionStorage.getItem("userData")).token.Api_token;
+  const [classeInfo, setClasseInfo] = useState([]);
+  useEffect(() => {
+    fetch(ENDPOINT("classes?classe=" + props.classe), getAuthRequest(token))
+      .then((r) => r.json())
+      .then((r) => {
+        if (r.status) setClasseInfo(r.response[0]);
+      });
+  }, [props.classe]);
+
   return (
     <Card.Body>
       <h2>{props.classe}</h2>
@@ -156,14 +211,24 @@ function Informations(props) {
         <h3>
           <u>Enseignant(e)</u>
         </h3>
-        Jean-Pierre Dubois
+        {classeInfo.Teacher || "Il n'y a pas d'enseignant"}
         <br />
         <h5>
           <u>
             <i>Assistant(e)</i>
           </u>
         </h5>
-        Julie Dubois
+        <ul>
+          {classeInfo.length < 1 ? (
+            <div>Chargement ...</div>
+          ) : classeInfo.assistants.length < 1 ? (
+            <div>Pas d'assistant</div>
+          ) : (
+            classeInfo.assistants.map((a) => (
+              <li key={classeInfo.assistants.indexOf(a)}>{a.Assistants}</li>
+            ))
+          )}
+        </ul>
       </div>
     </Card.Body>
   );
