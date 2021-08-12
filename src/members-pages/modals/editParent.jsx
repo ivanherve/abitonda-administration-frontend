6f1d040b78756371e0b3d7e092cf2fa50092c325
Ivan Herve <ivanherve1411@gmail.com>
2021-08-12 09:03:03 +0200

    edit parent

---
+++ src/members-pages/modals/editParent.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { ENDPOINT, postAuthRequestFormData } from "../../links/links";
+import { FrmGroupText } from "./editEmployee";
+
+export default function EditParent(props) {
+  const token = JSON.parse(sessionStorage.getItem("userData")).token.Api_token;
+  const parent = props.parent;
+  const [lastname, setLastname] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [phoneNumb, setPhoneNumb] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [linkChild, setLinkChild] = useState("");
+
+  const editParent = () => {
+    let data = new FormData();
+    data.append("ParentId", parent.ParentId);
+    data.append("Lastname", lastname);
+    data.append("Firstname", firstname);
+    data.append("PhoneNumb", phoneNumb);
+    data.append("Address", address);
+    data.append("Email", email);
+    data.append("LinkChild", linkChild);
+    fetch(ENDPOINT("editparent"), postAuthRequestFormData(data, token))
+      .then((r) => r.json())
+      .then((r) => {
+          console.log(r);
+      });
+  };
+
+  return (
+    <Modal show={props.show} onHide={props.hide} size="lg" centered>
+      <Modal.Header>
+        <Modal.Title>
+          Modifier {parent.Firstname} {parent.Lastname}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <FrmGroupText
+            controlId="formLastname"
+            label="Nom de famille"
+            placeholder={parent.Lastname}
+            change={(e) => setLastname(e.target.value)}
+          />
+          <FrmGroupText
+            controlId="formFirstname"
+            label="Prénom"
+            placeholder={parent.Firstname}
+            change={(e) => setFirstname(e.target.value)}
+          />
+          <FrmGroupText
+            controlId="formPhoneNumb"
+            label="Numéro de téléphone"
+            placeholder={parent.PhoneNumb}
+            change={(e) => setPhoneNumb(e.target.value)}
+          />
+          <FrmGroupText
+            controlId="formAddress"
+            label="Adresse Domicile"
+            placeholder={parent.Address}
+            change={(e) => setAddress(e.target.value)}
+          />
+          <FrmGroupText
+            controlId="formEmail"
+            label="E-mail"
+            placeholder={parent.Email}
+            change={(e) => setEmail(e.target.value)}
+          />
+          <FrmGroupText
+            controlId="formLinkChild"
+            label="Lien avec enfant"
+            placeholder={parent.LinkChild}
+            change={(e) => setLinkChild(e.target.value)}
+          />
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={() => editParent()}>Enregistrer</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
